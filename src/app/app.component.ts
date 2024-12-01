d01ed34f0ca2f598b83e842045d0253269ef072a
Franco Mosca <franco.mos2002@gmail.com>
2024-12-01 19:31:45 -0300

    feature/ tailwind and angular/fire config & installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'gestor-socios';
+  title = 'Gestor Socios - Club Florida Mitre';
 }
